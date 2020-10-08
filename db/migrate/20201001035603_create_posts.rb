4430b1b892230036c45b9b1465b3c365533d7539
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-08 13:14:00 +0900

    画像複数枚投稿の実装

--- db/migrate/20201001035603_create_posts.rb
+++ db/migrate/20201001035603_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration[6.0]
     create_table :posts do |t|
       t.string :title
       t.text :content
-      t.text :image
       t.timestamps
     end
   end
