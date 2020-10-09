9318cc5132accc753b2c43312ced90b61eaedd18
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-09 21:04:47 +0900

    fix

--- app/assets/javascript/mypage.js
+++ app/assets/javascript/mypage.js
@@ -13,3 +13,20 @@ $(function() {
 
   tabs.click(tabSwitch);
 });
+
+//follow・followerモーダル画面//=================================================================
+$(function() {
+  let tabs = $(".follow-nav");
+
+  function tabSwitch() {
+    $(".active").removeClass("active");
+
+    $(this).addClass("active");
+
+    const index = tabs.index(this);
+
+    $(".FollowPage").removeClass("show").eq(index).addClass("show");
+  }
+
+  tabs.click(tabSwitch);
+});
