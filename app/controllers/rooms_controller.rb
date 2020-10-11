00fe67552fc6672a0d6c100fddda9006252291dc
tomato1125 <naokiyoshida1125@icoud.com>
2020-10-11 15:31:32 +0900

    fix

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -16,17 +16,17 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
-        @messages = @room.messages.includes(:user).order("created_at asc")
-        @message = Message.new
-        @entries = @room.entries
+      @messages = @room.messages.includes(:user).order("created_at asc")
+      @message = Message.new
+      @entries = @room.entries
     else
-        redirect_back(fallback_location: root_path)
+      redirect_back(fallback_location: root_path)
     end
   end
 
   private
 
   def join_room_params
-      params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
+    params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 end
