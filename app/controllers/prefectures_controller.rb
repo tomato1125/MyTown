5dd5897359edb7cf8c761d00e86ee5686b903e1b
tomato1125 <naokiyoshida1125@icoud.com>
2020-11-11 16:48:43 +0900

    [add]prefecture-showpage-pagenation-function

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -3,5 +3,6 @@ class PrefecturesController < ApplicationController
   def show
     @prefecture = Prefecture.find(params[:prefecture_id])
     @prefectures = Post.where(prefecture_id: params[:prefecture_id]).order("created_at DESC")
+    @prefecturePosts = @prefectures.page(params[:page]).per(5).order("created_at DESC")
   end
 end
