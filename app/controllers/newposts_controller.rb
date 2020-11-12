28870058bf38fbfe05b6cb8744680ea8cafaef00
tomato1125 <naokiyoshida1125@icoud.com>
2020-11-12 14:16:52 +0900

    newpost-page-fix

--- app/controllers/newposts_controller.rb
+++ app/controllers/newposts_controller.rb
@@ -2,10 +2,5 @@ class NewpostsController < ApplicationController
   
   def index
     @posts = Post.page(params[:page]).per(5).order("created_at DESC")
-
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 end
